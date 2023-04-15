8b2afe1f7268005560d25fac6fc6031dab61568a
feb27 <feb27@10.0.0.123>
2023-04-15 00:11:29 -0400

    Initial commit

---
+++ src/hw2Project3/DemoProject3.java
@@ -0,0 +1,12 @@
+package hw2Project3;
+
+public class DemoProject3 {
+
+	public static void main(String args []) {
+		System.out.println("Demo 3..");
+	}
+	
+	public void sum(int a, int b) {
+		System.out.println(a+b);
+	}
+}
